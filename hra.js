297fef6ec2b1d9b7c7181a4508cd0b49ce74f5d5
Linda Hrdinová <hrlin@seznam.cz>
2023-10-03 19:50:09 +0200

    Základní splnění domácího úkolu

---
+++ hra.js
@@ -0,0 +1,29 @@
+const poleElement = document.querySelector('.piskvorkyHra__policko');
+const ikonaHracElement = document.querySelector('.piskvorkyHra__ikona--hrac');
+
+let hracNaTahu = 'kruh';
+
+const hracuvTah = (event) => {
+  if (hracNaTahu === 'kruh') {
+    event.target.classList.add('piskvorkyHra__policko--kruh');
+    event.target.disabled = true;
+    ikonaHracElement.src = 'cross.svg';
+    hracNaTahu = 'krizek';
+  } else if (hracNaTahu === 'krizek') {
+    event.target.classList.add('piskvorkyHra__policko--krizek');
+    event.target.disabled = true;
+    ikonaHracElement.src = 'circle.svg';
+    hracNaTahu = 'kruh';
+  }
+};
+
+document.querySelector('#pole00').addEventListener('click', hracuvTah);
+document.querySelector('#pole01').addEventListener('click', hracuvTah);
+document.querySelector('#pole02').addEventListener('click', hracuvTah);
+document.querySelector('#pole03').addEventListener('click', hracuvTah);
+document.querySelector('#pole04').addEventListener('click', hracuvTah);
+document.querySelector('#pole05').addEventListener('click', hracuvTah);
+document.querySelector('#pole06').addEventListener('click', hracuvTah);
+document.querySelector('#pole07').addEventListener('click', hracuvTah);
+document.querySelector('#pole08').addEventListener('click', hracuvTah);
+document.querySelector('#pole09').addEventListener('click', hracuvTah);
