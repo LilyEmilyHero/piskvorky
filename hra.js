567a18d3a1310a2c6aeafa91ca8e0dd385eb1455
Linda Hrdinová <hrlin@seznam.cz>
2023-10-09 18:41:09 +0200

    zpracování 2. bodu 4. úkolu

--- hra.js
+++ hra.js
@@ -1,8 +1,11 @@
-const poleElement = document.querySelector('.piskvorkyHra__policko');
+/* uložený prvků do proměných */
+const vsechnaPole = document.querySelectorAll('.piskvorkyHra__policko');
 const ikonaHracElement = document.querySelector('.piskvorkyHra__ikona--hrac');
 
+/* výchozí nastavení aktuálního hráce */
 let hracNaTahu = 'kruh';
 
+/* reakce na kliknutí na políčko */
 const hracuvTah = (event) => {
   if (hracNaTahu === 'kruh') {
     event.target.classList.add('piskvorkyHra__policko--kruh');
@@ -17,23 +20,19 @@ const hracuvTah = (event) => {
   }
 };
 
-document.querySelector('#pole00').addEventListener('click', hracuvTah);
-document.querySelector('#pole01').addEventListener('click', hracuvTah);
-document.querySelector('#pole02').addEventListener('click', hracuvTah);
-document.querySelector('#pole03').addEventListener('click', hracuvTah);
-document.querySelector('#pole04').addEventListener('click', hracuvTah);
-document.querySelector('#pole05').addEventListener('click', hracuvTah);
-document.querySelector('#pole06').addEventListener('click', hracuvTah);
-document.querySelector('#pole07').addEventListener('click', hracuvTah);
-document.querySelector('#pole08').addEventListener('click', hracuvTah);
-document.querySelector('#pole09').addEventListener('click', hracuvTah);
+/* výběr všech políček */
+vsechnaPole.forEach((pole) => {
+  pole.addEventListener('click', hracuvTah);
+});
 
+/* ošetření omylem restartované hry */
 const chceteRestartovatHru = (event) => {
   if (!confirm('Chcete restartovat hru?')) {
     event.preventDefault();
   }
 };
 
+/* kliknutí na restart */
 document
   .querySelector('.piskvorkyHra__tlacitka--restart')
   .addEventListener('click', chceteRestartovatHru);
